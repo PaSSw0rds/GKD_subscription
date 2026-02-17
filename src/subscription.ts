df47bc62be3608dbbdcee268ad379a75a2522c2b
chippy <17513790+PaSSw0rds@users.noreply.github.com>
2026-02-17 07:39:09 +0000

    chore: 吃了一些小鱼干

--- src/subscription.ts
+++ src/subscription.ts
@@ -16,12 +16,12 @@ apps.forEach((appConfig) => {
 });
 
 export default defineGkdSubscription({
-  id: 666,
-  name: 'AIsouler的GKD订阅-已停止维护',
+  id: 8534,
+  name: 'chippy的GKD订阅-已停止维护',
   version: 0,
-  author: 'AIsouler',
-  checkUpdateUrl: './AIsouler_gkd.version.json5',
-  supportUri: 'https://github.com/AIsouler/GKD_subscription/issues/new/choose',
+  author: 'chippy',
+  checkUpdateUrl: './chippy.version.json5',
+  supportUri: 'https://github.com/chippy/GKD_subscription/issues/new/choose',
   categories,
   globalGroups,
   apps: rawApps,
